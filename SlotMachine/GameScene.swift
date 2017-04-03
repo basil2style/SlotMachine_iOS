7cdec1d3a294fe9e1c66c2022e64923e076ea967
Basil <basiltalias92@gmail.com>
2017-04-03 15:31:15 -0400

    Added all values

--- SlotMachine/GameScene.swift
+++ SlotMachine/GameScene.swift
@@ -11,75 +11,99 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let slotOptions = ["banana","bell","flappy1","flappy2","orange"]
+    
+    var currentWheelStringValue1:String = ""
+    var currentWheelStringValue2:String = ""
+    var currentWheelStringValue3:String = ""
+    
+    var bg = SKSpriteNode()
+    var wheelActive:Bool = false
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        let bgTexture = SKTexture(imageNamed: "slotmachine.png");
+        bg = SKSpriteNode(texture: bgTexture)
+        //bg.position = CGPoint(x: bgTexture.size().width * i, y: self.frame.midY)
+        bg.size.height = self.frame.height
+        bg.size.width = self.frame.width
+        bg.zPosition = -1
+        self.addChild(bg)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        
+        if(wheelActive == false){
+            print("spinning");
+            
+            let wait:SKAction = SKAction.wait(forDuration: 1)
+            let spinWheel1:SKAction = SKAction.run({
+                self.spinWheel(whichWheel: 1)
+            })
+            let spinWheel2:SKAction = SKAction.run({
+                self.spinWheel(whichWheel: 2)
+            })
+            let spinWheel3:SKAction = SKAction.run({
+                self.spinWheel(whichWheel: 3)
+            })
+            let testWheelValues:SKAction = SKAction.run({
+                self.testValues()
+            })
+            
+            self.run(SKAction.sequence([wait,spinWheel1,wait,spinWheel2,wait,spinWheel3,wait,testWheelValues]))
+            
+        }
+    }
+    
+    func testValues()  {
+        let winLabel:SKLabelNode = self.childNode(withName: "winLabel") as! SKLabelNode
+        if (currentWheelStringValue1 == currentWheelStringValue2 && currentWheelStringValue2 == currentWheelStringValue3) {
+            
+            winLabel.text = "WIN"
+            
+            print("You won")
+        }else {
+            winLabel.text = "LOSS"
+            print("Please play again")
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func spinWheel(whichWheel:Int)  {
+        let randomNum:UInt32 = arc4random_uniform(UInt32(slotOptions.count))
+        let wheelPick:String = slotOptions[Int(randomNum)]
+        print("Wheel \(wheelPick) spun a value of \(whichWheel)")
+        
+        if (whichWheel == 1) {
+            currentWheelStringValue1 = wheelPick
+            
+            if (self.childNode(withName: "wheel1") != nil) {
+                if let wheel1:SKSpriteNode = self.childNode(withName: "wheel1") as! SKSpriteNode {
+                    wheel1.texture = SKTexture(imageNamed:wheelPick)
+                }
+                //self.childNode(withName: "wheel1").texture = SKTexture(imageNamed:wheelPick)
+            }
+            
+        } else if (whichWheel == 2) {
+            currentWheelStringValue2 = wheelPick
+           // sprite1.texture = SKTexture(imageNamed:wheelPick)
+            if (self.childNode(withName: "wheel2") != nil) {
+                if let wheel2:SKSpriteNode = self.childNode(withName: "wheel2") as! SKSpriteNode {
+                    wheel2.texture = SKTexture(imageNamed:wheelPick)
+                }
+                //self.childNode(withName: "wheel1").texture = SKTexture(imageNamed:wheelPick)
+            }
+            
+        } else if (whichWheel == 3) {
+            currentWheelStringValue3 = wheelPick
+           // sprite1.texture = SKTexture(imageNamed:wheelPick)
+            if (self.childNode(withName: "wheel3") != nil) {
+                if let wheel1:SKSpriteNode = self.childNode(withName: "wheel3") as! SKSpriteNode {
+                    wheel1.texture = SKTexture(imageNamed:wheelPick)
+                }
+                //self.childNode(withName: "wheel1").texture = SKTexture(imageNamed:wheelPick)
+            }
+        }
     }
     
     
